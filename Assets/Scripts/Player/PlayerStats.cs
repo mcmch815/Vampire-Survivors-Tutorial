7e14c705f84651a0a5045454929e50f793979c9d
Christopher McMahon <cjtronics@gmail.com>
2025-03-11 08:19:15 +1300

    Advanced Complete

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
@@ -9,6 +11,22 @@ public class PlayerStats : MonoBehaviour
     float currentRecovery;
     float currentProjectileSpeed;
 
+    //Exerperience and level of player
+    [Header("Experience and Level")]
+    public int experience = 0;
+    public int level = 1;
+    public int experienceCap;
+
+    [System.Serializable]
+    public class LevelRange
+    {
+        public int startLevel;
+        public int endLevel;
+        public int experienceCapIncrease;
+    }
+
+    public List<LevelRange> levelRanges;
+    
     void Awake()
     {
         currentHealth = characterData.MaxHealth;
@@ -16,6 +34,36 @@ public class PlayerStats : MonoBehaviour
         currentSpeed = characterData.MoveSpeed;
         currentRecovery = characterData.Recovery;
         currentProjectileSpeed = characterData.ProjectileSpeed;
+    }
+
+    void Start()
+    {
+        experienceCap = levelRanges[0].experienceCapIncrease;
+    }
+
+    public void IncreaseExperience(int amount)
+    {
+        experience += amount;
+        LevelUpChecker();
+    }
+
+    void LevelUpChecker()
+    {
+        if (experience >= experienceCap)
+        {
+            level++;
+            experience -= experienceCap;
+            int experienceCapIncrease = 0;
+            foreach (LevelRange levelRange in levelRanges)
+            {
+                if (level >= levelRange.startLevel && level <= levelRange.endLevel)
+                {
+                    experienceCapIncrease = levelRange.experienceCapIncrease;
+                    break;
+                }
+            }
+            experienceCap += experienceCapIncrease;
+        }
     }
   
 }
